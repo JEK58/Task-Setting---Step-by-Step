3929e8943d4f60648db12085e64635c4b75fc4ba
Stephan Schöpe <socialcrap+github@posteo.de>
2023-10-22 14:34:34 +0200

    correct deploy link added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://tasksetting.vercel.app/",
   integrations: [mdx()],
 });
