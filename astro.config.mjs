7da008df39207f19dcae2cbcae85f7c14ed32c29
Stephan Schöpe <socialcrap+github@posteo.de>
2023-10-23 12:29:55 +0200

    analytics added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tasksetting.vercel.app/",
   integrations: [mdx()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
