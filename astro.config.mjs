30e6dcf476061bd92bd3d047f1e941d20c88008f
Stephan Schöpe <socialcrap+github@posteo.de>
2023-10-30 23:46:36 +0100

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,41 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/static";
+import AstroPWA from "@vite-pwa/astro";
+import { SITE_TITLE, SITE_DESCRIPTION } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tasksetting.vercel.app/",
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    AstroPWA({
+      registerType: "autoUpdate",
+      // base: "/",
+      // scope: "/",
+      manifest: {
+        name: SITE_TITLE,
+        short_name: "Task Setting",
+        description: SITE_DESCRIPTION,
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   output: "static",
   adapter: vercel({
     webAnalytics: {
