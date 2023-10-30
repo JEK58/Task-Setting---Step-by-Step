01a154548530f397adcb1e2cdcedc658ac969fd6
Stephan Schöpe <socialcrap+github@posteo.de>
2023-10-30 16:32:29 +0100

    pwa support added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/static";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tasksetting.vercel.app/",
-  integrations: [mdx()],
+  integrations: [mdx(), AstroPWA()],
   output: "static",
   adapter: vercel({
     webAnalytics: {
